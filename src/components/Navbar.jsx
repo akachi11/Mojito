023407f67f2ca87e19be650fc16ffece75085e7d
akachi11 <ogogorchimadika6@gmail.com>
2026-07-27 00:12:04 +0100

    Implement Art and Menu Sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
 import gsap from 'gsap';
-import {navLinks} from "../../constants/index.js";
-import {useGSAP} from "@gsap/react";
+import {useGSAP} from '@gsap/react'
+
+import {navLinks} from '../../constants/index.js'
 
 const Navbar = () => {
-
     useGSAP(() => {
         const navTween = gsap.timeline({
             scrollTrigger: {
-                trigger: " nav",
-                start: "bottom top"
+                trigger: 'nav',
+                start: 'bottom top'
             }
-        })
+        });
 
-        navTween.fromTo("nav", {backgroundColor: "transparent"}, {
-            backgroundColor: "#00000050",
-            backgroundFilter: "blur(10px)",
+        navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
             duration: 1,
-            ease: "power1.inOut"
-        })
+            ease: 'power1.inOut'
+        });
     })
 
     return (
         <nav>
             <div>
-                <a href={"#home"} className={"flex items-center gap-2"}>
-                    <img src={"/images/logo.png"} alt={"logo"}/>
+                <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="logo"/>
                     <p>Velvet Pour</p>
                 </a>
 
@@ -38,7 +37,6 @@ const Navbar = () => {
                 </ul>
             </div>
         </nav>
-    );
-};
-
-export default Navbar;
+    )
+}
+export default Navbar
